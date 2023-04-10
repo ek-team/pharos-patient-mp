071603785f3bd1074f8ace98106bdcd0eed88c5f
renjie117 <1173847181@qq.com>
2023-04-10 13:33:11 +0800

    代码提交
    1.注册手机号限制去除 2.回收单物品重量默认为25kg 3.服务详情出去团队人员

--- miniprogram/pages/addPatient/addPatient.js
+++ miniprogram/pages/addPatient/addPatient.js
@@ -149,7 +149,9 @@ Page({
         })
         return ; 
       }
-      if(!(/^1[3|4|5|7|8]\d{9}$/.test(this.data.phone))){
+      // if(!(/^1[2|3|4|5|6|7|8|9]\d{9}$/.test(this.data.phone))){
+
+      if(this.data.phone.length!=11){
         wx.showToast({
           title: '请输入正确的手机号',
           icon: 'none'
